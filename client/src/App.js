99f3fd7e4b1f599f476611bbb03bf70621727bc3
Jessica Embury <juannajane@Jessicas-MacBook-Air.local>
2021-02-19 11:33:39 -0800

    move footer

--- client/src/App.js
+++ client/src/App.js
@@ -47,6 +47,13 @@ function App() {
 
   return (
     <div className="header-div">
+
+<footer><a target='_blank' href='https://github.com/dollydoxyfree'><img height='20px' width='20px' src='https://res.cloudinary.com/dollydoxyfree/image/upload/v1613762248/Screen_Shot_2021-02-19_at_11.13.48_AM_g7uuop.png' alt='github logo' /></a>
+        
+        <a target='_blank' href='https://www.linkedin.com/in/jessica-embury-49027b204/'><img height='20px' width='20px' src='https://res.cloudinary.com/dollydoxyfree/image/upload/v1613762250/Screen_Shot_2021-02-19_at_11.16.15_AM_qmqssr.png' alt='github logo' /></a>
+        
+      </footer>
+      
       <div className="title-container">
         <header>
           <div className="ragePage">
@@ -143,11 +150,11 @@ function App() {
           ))}
         </div>
       </Route><br/>
-      <footer><a target='_blank' href='https://github.com/dollydoxyfree'><img height='25px' width='25px' src='https://res.cloudinary.com/dollydoxyfree/image/upload/v1613762248/Screen_Shot_2021-02-19_at_11.13.48_AM_g7uuop.png' alt='github logo' /></a>
+      {/* <footer><a target='_blank' href='https://github.com/dollydoxyfree'><img height='25px' width='25px' src='https://res.cloudinary.com/dollydoxyfree/image/upload/v1613762248/Screen_Shot_2021-02-19_at_11.13.48_AM_g7uuop.png' alt='github logo' /></a>
         
       <a target='_blank' href='https://www.linkedin.com/in/jessica-embury-49027b204/'><img height='25px' width='25px' src='https://res.cloudinary.com/dollydoxyfree/image/upload/v1613762250/Screen_Shot_2021-02-19_at_11.16.15_AM_qmqssr.png' alt='github logo' /></a>
       
-      </footer>
+      </footer> */}
    
     </div>
 
