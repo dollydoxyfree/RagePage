aaf4aa7df25c6dcd3f3a5fc24a7b8c73ae948d62
Jessica Embury <juannajane@Jessicas-MacBook-Air.local>
2021-02-22 11:15:27 -0800

    fire background

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -7,16 +7,16 @@ function Nav() {
   return (
     <nav>
            <Link to='/'>
-     <h4 className='navcontent'>HOME <br/></h4>
+     <h4 className='navcontent'>HOME   <br/></h4>
       </Link>
       <Link to='/relationships'>
-      <h4 className='navcontent'>RELATIONSHIPS </h4>
+      <h4 className='navcontent'>  RELATIONSHIPS   </h4>
       </Link>
        <Link to='/hospitality'>
-      <h4 className='navcontent'>BUSINESS/HOSPITALITY </h4>
+      <h4 className='navcontent'>  BUSINESS/HOSPITALITY   </h4>
       </Link>
       <Link to='/road-rage'>
-      <h4 className='navcontent'>ROADRAGE</h4>
+      <h4 className='navcontent'>  ROADRAGE</h4>
       </Link>
  </nav>
   )
