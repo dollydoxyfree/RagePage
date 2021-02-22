e8b1b54132c49bf03402d118191f3ec160a1fbbd
Jessica Embury <juannajane@Jessicas-MacBook-Air.local>
2021-02-22 09:08:16 -0800

    hover effect

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -7,13 +7,13 @@ function Nav() {
   return (
     <nav>
            <Link to='/'>
-     <h4 className='navcontent'>HOME<br/></h4>
+     <h4 className='navcontent'>HOME <br/></h4>
       </Link>
       <Link to='/relationships'>
-      <h4 className='navcontent'>RELATIONSHIPS</h4>
+      <h4 className='navcontent'>RELATIONSHIPS </h4>
       </Link>
        <Link to='/hospitality'>
-      <h4 className='navcontent'>BUSINESS/HOSPITALITY</h4>
+      <h4 className='navcontent'>BUSINESS/HOSPITALITY </h4>
       </Link>
       <Link to='/road-rage'>
       <h4 className='navcontent'>ROADRAGE</h4>
